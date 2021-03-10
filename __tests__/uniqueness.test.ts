f36a183227f9a8833c49c263665ae63e82b75ea7
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-03-10 22:49:16 +0530

    improved code clarity

--- __tests__/uniqueness.test.ts
+++ __tests__/uniqueness.test.ts
@@ -1,12 +1,12 @@
 import { map } from '../src/mapping';
 
-let mySet = new Set();
+const uniqueCharacters = new Set();
 
 describe('Testing uniqueness', () => {
     Object.keys(map).forEach(key => {
-        mySet.add(key);
+        uniqueCharacters.add(key);
     });
     it('should take in mapping, add keys to a set and compare size', () => {
-        expect(Object.keys(map).length).toEqual(mySet.size);
+        expect(Object.keys(map).length).toEqual(uniqueCharacters.size);
     });
 });
